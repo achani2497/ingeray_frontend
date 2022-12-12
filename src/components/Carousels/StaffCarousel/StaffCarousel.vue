c71b53fa58ee913493848af8c91f6e97a40c360f
Ale Chani <achani@est.frba.utn.edu.ar>
2022-12-12 00:38:22 -0300

    Dejo terminando la seccion Quienes Somos, faltan detalles

---
+++ src/components/Carousels/StaffCarousel/StaffCarousel.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="flex flex-col gap-8 relative">
+    <titles :title="title" :subtitle="subtitle" class="px-16"></titles>
+    <div class="carousel-container flex items-center">
+      <!-- Control button Left -->
+      <button
+        class="h-6 w-6 mr-8 arrow left rounded-full"
+        @click="prev()"
+      ></button>
+      <!-- Carousel -->
+      <div class="carousel px-14">
+        <div class="overflow-hidden flex">
+          <div class="inner flex gap-4 pb-4" ref="inner" :style="innerStyles">
+            <div
+              class="card-container"
+              v-for="(person, index) in carouselElements"
+              :key="index"
+            >
+              <img
+                :src="
+                  require(`@/assets/images/quienes-somos/staff-cards/${person.image}`)
+                "
+                width="290"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Control button right -->
+      <button class="h-6 w-6 arrow rounded-full" @click="next()"></button>
+    </div>
+    <a
+      href="https://ingeraysrl.mercadoshops.com.ar/"
+      class="absolute right-12 -bottom-6 rounded-full"
+      v-if="showCheckout"
+    >
+      <img
+        class="inge-shadow-down rounded-full"
+        :src="require('@/assets/images/icons/checkout.png')"
+        alt="carrito-de-compras"
+        width="70"
+      />
+    </a>
+  </div>
+</template>
+<style scoped >
+.card-container {
+  width: 270px;
+  padding-left: 5px;
+  display: flex;
+}
+.carousel-container {
+  width: 100%;
+  position: relative;
+}
+.carousel {
+  width: 100vw;
+  overflow: hidden;
+}
+.inner {
+  width: fit-content;
+  transition: transform 0.2s;
+}
+button {
+  margin-right: 0px;
+  margin-top: 10px;
+}
+.arrow {
+  background-image: url("~@/assets/images/icons/arrow.svg");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  transform: scale(3);
+}
+.left {
+  transform: rotate(-180deg) scale(3);
+}
+</style>
+<script>
+import { staff } from "@/assets/js/quienes-somos.json";
+
+export default {
+  data() {
+    return {
+      innerStyles: {},
+      stepSize: "",
+      transitioning: false,
+      carouselElements: staff,
+    };
+  },
+  mounted() {
+    this.setStep();
+    this.resetTranslate();
+  },
+  methods: {
+    setStep() {
+      this.stepSize = `${275 + 16}px`;
+    },
+    next() {
+      if (this.transitioning) return;
+      this.transitioning = true;
+      this.moveLeft();
+      this.afterTransition(() => {
+        const card = this.carouselElements.shift();
+        this.carouselElements.push(card);
+        this.resetTranslate();
+        this.transitioning = false;
+      });
+    },
+    prev() {
+      if (this.transitioning) return;
+      this.transitioning = true;
+      this.moveRight();
+      this.afterTransition(() => {
+        const card = this.carouselElements.pop();
+        this.carouselElements.unshift(card);
+        this.resetTranslate();
+        this.transitioning = false;
+      });
+    },
+    moveLeft() {
+      this.innerStyles = {
+        transform: `translateX(-${this.stepSize})
+                          translateX(-${this.stepSize})`,
+      };
+    },
+    moveRight() {
+      this.innerStyles = {
+        transform: `translateX(${this.stepSize})
+                        translateX(-${this.stepSize})`,
+      };
+    },
+    afterTransition(callback) {
+      const listener = () => {
+        callback();
+        this.$refs.inner.removeEventListener("transitionend", listener);
+      };
+      this.$refs.inner.addEventListener("transitionend", listener);
+    },
+    resetTranslate() {
+      this.innerStyles = {
+        transition: "none",
+        transform: `translateX(-${this.stepSize})`,
+      };
+    },
+    getImage(imagePath) {
+      return require(`@/assets/images/home/${imagePath}.jpg`);
+    },
+  },
+};
+</script>
--- src/views/QuienesSomos.vue
+++ src/views/QuienesSomos.vue
@@ -1,95 +1,298 @@
 <template>
-    <div class="flex flex-col gap-4 p-6">
-        <div class="text-container flex flex-col">
-            <div class="title blue">
-                Misión
+  <div class="flex flex-col gap-8">
+    <div class="padding-container flex flex-col gap-16">
+      <banner-with-bot
+        :bannerName="`${this.$imageCDN}/banner-quienes-somos.jpg`"
+      >
+        <template slot="title">
+          <titles
+            title="Conocé a nuestra empresa y a los que forman parte"
+            subtitle="Nos enfocamos en brindar soluciones"
+            :fontSize="30"
+          ></titles>
+        </template>
+        <template slot="stuff">
+          <p class="text-justify text-xl leading-9">
+            Desde 1992 INGE RAY SRL se especializa en Equipamiento para
+            Diagnóstico por Imágenes y al día de hoy somos la empresa del área
+            más compenetrada con las necesidades de nuestros clientes. Desde ese
+            lugar brindamos Soluciones Tecnológicas, Técnicas y Comerciales
+            adecuadas que permitan ofrecer diagnósticos de calidad. Somos un
+            equipo de profesionales diverso, donde cada uno tiene su lugar
+            siendo valorado, respetado y capacitado en su área de trabajo para
+            dar respuestas concretas a las necesidades de los Servicios para la
+            Salud.
+          </p>
+          <button
+            @click="showContactMenu = true"
+            class="banner-contacto flex items-center gap-2 mb-10 -mt-2"
+          >
+            <div class="envelope"></div>
+            <div class="text">
+              <titles
+                title="Tengo una consulta"
+                subtitle="Quiero que me contacten"
+                class="banner-contacto-title"
+                :fontSize="16"
+                :lineHeight="10"
+              ></titles>
             </div>
-            <p class="simple-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quisquam minus ipsa laudantium ullam nobis blanditiis maiores sint totam illo hic harum recusandae deleniti accusamus, saepe delectus sapiente repellendus sed consequatur est tempore! Exercitationem perferendis illo excepturi quae laudantium architecto accusamus mollitia quo natus? Dolores tempora eos nulla consectetur ducimus.
-            </p>
-        </div>
-        <div class="text-container flex flex-col">
-            <div class="title blue">
-                Visión
-            </div>
-            <p class="simple-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quisquam minus ipsa laudantium ullam nobis blanditiis maiores sint totam illo hic harum recusandae deleniti accusamus, saepe delectus sapiente repellendus sed consequatur est tempore! Exercitationem perferendis illo excepturi quae laudantium architecto accusamus mollitia quo natus? Dolores tempora eos nulla consectetur ducimus.
-            </p>
-        </div>
-        <div class="text-container flex flex-col">
-            <div class="title blue">
-                Objetivo
-            </div>
-            <p class="simple-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quisquam minus ipsa laudantium ullam nobis blanditiis maiores sint totam illo hic harum recusandae deleniti accusamus, saepe delectus sapiente repellendus sed consequatur est tempore! Exercitationem perferendis illo excepturi quae laudantium architecto accusamus mollitia quo natus? Dolores tempora eos nulla consectetur ducimus.
-            </p>
-        </div>
-        <div class="staff">
-            <div class="title blue">Conocé a Nuestro Equipo</div>
-            <div class="staff-personal">
-                <!-- Card -->
-                <div class="personal-card bkg-light-gray" v-for="(persona, index) in staff" :key="index">
-                    <div class="personal-card-header flex items-center justify-center p-4">
-                        <img :src="persona.foto" alt="img">
-                    </div>
-                    <div class="personal-card-body p-4 flex flex-col">
-                        <div class="name blue">
-                            {{persona.nombre}}
-                        </div>
-                        <div class="job-title blue">
-                            {{persona.rol}}
-                        </div>
-                        <p>
-                            {{persona.descripcion}}
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
+          </button>
+        </template>
+      </banner-with-bot>
+      <!-- Bases -->
+      <div class="flex flex-col gap-4">
+        <titles
+          title="Las bases que"
+          subtitle="Nos inspiran para crecer"
+        ></titles>
+        <mision-vision-valores></mision-vision-valores>
+      </div>
     </div>
+    <!-- Company`s Staff -->
+    <div class="padding-container py-16 bg-white">
+      <div class="flex flex-col gap-10" v-if="!showAllTeam">
+        <titles
+          class="pl-3"
+          title="Conocénos"
+          subtitle="Somos un gran equipo"
+        ></titles>
+        <div class="staff-description-card flex">
+          <img
+            :src="
+              require('@/assets/images/contacto/fotosPersonal/daniel_penhos.jpg')
+            "
+            width="310"
+          />
+          <div class="flex flex-col gap-3 px-16 py-12">
+            <div>
+              <h2 class="inge-text-bold text-3xl mb-2">Daniel Penhos</h2>
+              <titles subtitle="Gerente Operativo"></titles>
+              <titles title="Área Técnico Comercial" :fontSize="24"></titles>
+            </div>
+            <p class="text-xl leading-8 text-justify">
+              Ingeniero en Electrónica recibido en la Universidad Tecnológica
+              Nacional con Especialización en Control de Procesos. Socio
+              Fundador de nuestra empresa, se desarrolla en el Área de Diseño y
+              Desarrollo Tecnológico, Relaciones Institucionales y Comercio
+              Exterior.
+            </p>
+            <a
+              href="https://ar.linkedin.com/in/daniel-penhos-42b87a43"
+              target="_blank"
+              class="blue"
+              >Conozca más sobre nuestro Gerente Operativo</a
+            >
+          </div>
+        </div>
+        <div class="staff-description-card flex">
+          <img
+            :src="
+              require('@/assets/images/contacto/fotosPersonal/matias_penhos.jpg')
+            "
+            width="310"
+          />
+          <div class="flex flex-col gap-3 px-16 py-12">
+            <div>
+              <h2 class="inge-text-bold text-3xl mb-2">Matías Penhos</h2>
+              <titles subtitle="Gerente Operativo"></titles>
+              <titles title="Área Técnico Comercial" :fontSize="24"></titles>
+            </div>
+            <p class="text-xl leading-8 text-justify">
+              Técnico Electromecánico escpecializado en el Área de Relevamiento
+              Tecnológico y Soluciones a Medida. Aseror Técnico Comercial.
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- Extended team Chiefs & Staff -->
+      <div class="flex flex-col gap-12" v-else>
+        <titles
+          title="El equipo de Inge Ray está compuesto por un"
+          subtitle="Grupo Multidiciplinario de Profesionales que disfrutan su trabajo"
+        ></titles>
+        <!-- Gerentes -->
+        <div class="flex justify-center gap-6">
+          <presentation-card
+            v-for="(chief, index) in chiefs"
+            :person="chief"
+            :key="index"
+          ></presentation-card>
+        </div>
+        <!-- Staff -->
+        <div class="presentation-cards">
+          <presentation-card
+            v-for="(person, index) in staff"
+            :person="person"
+            :key="index + 2"
+          >
+          </presentation-card>
+        </div>
+      </div>
+    </div>
+    <!-- Staff carousel -->
+    <div
+      class="padding-container bkg-contracts -mt-8 py-16"
+      v-if="!showAllTeam"
+    >
+      <div class="flex flex-col gap-8">
+        <titles
+          title="El equipo de Inge Ray está compuesto por un"
+          subtitle="Grupo Multidiciplinario de Profesionales que disfrutan su trabajo"
+        ></titles>
+        <staff-carousel></staff-carousel>
+        <button
+          class="contact-inge-button self-end"
+          @click="showAllTeam = true"
+        >
+          Ver todo el equipo
+        </button>
+      </div>
+    </div>
+    <!-- Extended team (Contracts) -->
+    <div class="padding-container bkg-contracts -mt-8" v-if="showAllTeam">
+      <div class="presentation-cards py-16">
+        <presentation-card
+          v-for="(contract, index) in contracts"
+          :person="contract"
+          :key="index"
+        ></presentation-card>
+      </div>
+    </div>
+    <!-- Memorial -->
+    <div
+      class="padding-container bg-white -mt-8 flex flex-col gap-8 py-16"
+      v-if="showAllTeam"
+    >
+      <titles title="Conozca más sobre" subtitle="Nuestra Historia"></titles>
+      <p class="text-lg">
+        En INGE RAY SRL reconocemos a todos los que formaron parte en estos 30
+        años, los recordamos con alegría y les agradecemos por haber caminado
+        juntos para hacer posible nuestro presente.
+      </p>
+      <h3 class="inge-text-bold text-xl gray-text">
+        JUAN CARLOS ROMERO, MARTA PENHOS, JOSÉ PENHOS, ENRIQUE DEL BUENO,
+        VIRGINIA CORRALES, GABRIELA PENHOS, FERNANDO DANIEL MENOTTI, LUCIO DEL
+        BUENO.
+      </h3>
+    </div>
+    <!-- Valores -->
+    <div class="bg-white -mt-8 py-16">
+      <text-checklist
+        title="Conozca más sobre"
+        subtitle="Los valores que nos mueven"
+        :list="valores"
+        :extraPadding="false"
+      >
+        <template v-slot:button>
+          <a
+            href="mailto:rrhh@ingeray.com.ar"
+            class="contact-inge-button self-end py-2 px-8"
+          >
+            Sumate al equipo
+          </a>
+        </template>
+      </text-checklist>
+    </div>
+    <!-- Banner de Quienes Somos -->
+    <img
+      :src="`${this.$imageCDN}/banner_colaboradores.jpg`"
+      alt="Banner de Colaboradores"
+      class="-mt-8"
+    />
+    <!-- Certificaciones -->
+    <div class="padding-container flex flex-col gap-4 -mt-8 py-16 bg-white">
+      <titles
+        title="Certificaciones ante"
+        subtitle="Organismos Oficiales"
+        :fontSize="25"
+        class="pl-8"
+      ></titles>
+      <ul class="flex flex-wrap justify-around">
+        <li>
+          <img
+            src="@/assets/images/logos-clientes/ministerio-de-salud.png"
+            alt="logo del ministerio de salud de la nación"
+            width="250"
+          />
+        </li>
+        <li>
+          <img
+            src="@/assets/images/logos-clientes/gba.png"
+            alt="logo del gobierno de buenos aires"
+            width="250"
+          />
+        </li>
+        <li>
+          <img
+            src="@/assets/images/logos-clientes/gcba.png"
+            alt="logo de logo del gobierno de la ciudad de buenos aires"
+            width="250"
+          />
+        </li>
+      </ul>
+      <img
+        src="@/assets/images/logos-clientes/anmat.png"
+        alt="logo de anmat"
+        width="250"
+        class="self-center"
+      />
+    </div>
+  </div>
 </template>
 <style scoped>
-.staff-personal{
-    display: grid;
-    gap: 1rem;
-    grid-auto-rows: auto;
-    grid-template-columns: repeat(auto-fill, minmax(18rem, 1fr));
+.banner-contacto {
+  height: 50px;
+  background-color: #e6e6e6;
+  padding: 2rem 4rem;
+  width: 100%;
 }
-.personal-card{
-    box-shadow: 0px 3px 5px 2px rgba(0,0,0,.3);
+.staff-description-card {
+  background-color: #efefef;
 }
-img{
-    height: 200px;
-    width: 200px;
-    border-radius: 50%;
+.staff-description-card img {
+  object-fit: contain;
 }
-.title{
-    margin: 0;
+.presentation-cards {
+  display: grid;
+  gap: 3rem;
+  grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
+  justify-items: center;
 }
-.name{
-    font-size: 30px;
-    height: 35px;
+.bkg-contracts {
+  background-color: #c8c8c8;
 }
-.job-title{
-    font-size: 23px;
-}
-.personal-card-body p{
-    font-size: 18px;
+.gray-text {
+  color: #666666;
 }
 </style>
 <script>
+import BannerWithBot from "../components/Common/BannerWithBot.vue";
+import MisionVisionValores from "../components/QuienesSomos/MisionVisionValores.vue";
+import PresentationCard from "../components/QuienesSomos/PresentationCard.vue";
+import { chiefs, staff, contracts } from "../assets/js/quienes-somos.json";
+import TextList from "../components/Common/TextList.vue";
+import { valores } from "@/assets/js/valores.json";
+import StaffCarousel from "../components/Carousels/StaffCarousel/StaffCarousel.vue";
+
 export default {
-    data(){
-        return{
-            staff: []
-        }
-    },
-    mounted(){
-        fetch("/staff.json") //El archivo tiene que estar en la carpeta public
-            .then(r => r.json())
-            .then(json => {
-                this.staff = json.staff
-            })
-    }    
-}
+  components: {
+    "banner-with-bot": BannerWithBot,
+    "mision-vision-valores": MisionVisionValores,
+    "presentation-card": PresentationCard,
+    "text-checklist": TextList,
+    "staff-carousel": StaffCarousel,
+  },
+  data() {
+    return {
+      staff: [],
+      showContactMenu: false,
+      showAllTeam: false,
+      chiefs,
+      staff,
+      contracts,
+      valores,
+    };
+  },
+};
 </script>
