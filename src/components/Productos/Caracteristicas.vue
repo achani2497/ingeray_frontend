d5d1f47e813dd745c9f4cd42a2beba3084311cb3
Maximiliano Frega <maxifrega21@gmail.com>
2022-12-14 14:55:52 -0300

    corregida seccion caracteristitcas

--- src/components/Productos/Caracteristicas.vue
+++ src/components/Productos/Caracteristicas.vue
@@ -4,20 +4,56 @@
             <titles title="Características" :subtitle="datos.titulo" :fontSize="25"></titles>
         </div>
         <div class="texto-caracteristica simple-text py-4">
-            {{(datos.content)}}
+            <div class="custom-col-container">
+                <div class="custom-col">
+                    {{primeraMitad(datos.content)}}
+                </div>
+                <div class="custom-col">
+                    <img v-if="datos.imagen" :src="getImgSource(datos.imagen)" alt="img-caracteristicas">
+                    {{segundaMitad(datos.content)}}
+                </div>
+            </div>            
         </div>
         <button class="contact-outline">Ver Más</button>
     </div>
 </template>
 <script>
     export default {
-        props:['datos']
+        props:['datos'],
+        data() {
+            return {
+                mediaTexto: 0,
+            }
+        },
+        methods: {
+            getImgSource: function(imgSrc) {
+                return require(`@/assets/images/productos/humanos/${imgSrc}`)
+            },
+            primeraMitad: function(texto) {
+                let textLength = texto.length;
+                this.mediaTexto = Math.floor(textLength*0.6);
+                return texto.substring(0, this.mediaTexto);
+            },
+            segundaMitad: function(texto) {
+                return texto.substring(this.mediaTexto)
+            }
+        },
     }
 </script>
 <style scoped>
 .texto-caracteristica{
-    column-count: 2;
     text-align: justify;
+}
+.custom-col-container{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+.custom-col {
+    display: flex;
+    width: 45%;
+    flex-direction: column;
+    justify-content: stretch;
 }
 @media screen and (max-width: 650px){
     .texto-caracteristica{
