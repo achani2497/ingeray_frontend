485a00988b40d4b3cc171253e3e3e42895631bd3
Maximiliano Frega <maxifrega21@gmail.com>
2022-12-29 15:25:09 -0300

    cambios para descargar brochures

--- src/components/Productos/Documentos.vue
+++ src/components/Productos/Documentos.vue
@@ -1,5 +1,5 @@
 <template>
-    <!-- <div class="documentos flex flex-col gap-4 py-4">
+    <div class="documentos flex flex-col gap-4 py-4">
         <div class="titulos">
             <titles title="Brochure" subtitle="Documentos" :fontSize="25"></titles>
         </div>
@@ -7,16 +7,21 @@
             <div class="brochures flex flex-col gap-4">
                 <div v-for="datos in brochures" class="flex items-center gap-3">
                     <div class="icono-brochure"></div>
-                    <a :href="datos.link">{{datos.nombreLink}}</a>
+                    <a v-if="datos.link != '#'" :href="getPdfUrl(datos.link)" download>{{datos.nombreLink}}</a>
                 </div>
             </div>
             <router-link to="/productos" class="contact-outline">Ver todos los productos</router-link>
         </div>
-    </div> -->
+    </div>
 </template>
 <script>
 export default {
-    props: ['brochures']
+    props: ['brochures'],
+    methods: {
+        getPdfUrl: function(direccion) {
+            return `@/assets/documentos/${direccion}`
+        }
+    }
 }
 </script>
 <style scoped>
