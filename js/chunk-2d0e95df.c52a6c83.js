b4083dd79c09f121ef0cea0c5c139283e265f097
Max-021 <70070976+Max-021@users.noreply.github.com>
2023-02-17 16:08:07 -0300

    Delete js directory

--- js/chunk-2d0e95df.c52a6c83.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e95df"],{"8cdb":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"padding-container bg-red-500 h-48"},[t("h1",{staticClass:"mt-20"},[e._v("Ups no existe nada acá")])])}],c={},i=c,r=t("2877"),l=Object(r["a"])(i,a,s,!1,null,null,null);n["default"]=l.exports}}]);
-//# sourceMappingURL=chunk-2d0e95df.c52a6c83.js.map
