91fa1d425fc5aaa4d8edfa639180fd7b77e8fe1f
Maximiliano Frega <maxifrega21@gmail.com>
2023-05-16 22:04:27 -0300

    cambios en

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/ingeray_frontend/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '' : '/'
 }
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production' ? '/ingeray_frontend/' : '/'
+// }
