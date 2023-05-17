b17505ba409d57590df42680dd9546c4ac383833
Maximiliano Frega <maxifrega21@gmail.com>
2023-05-17 11:35:42 -0300

    cambios en router

--- src/router/index.js
+++ src/router/index.js
@@ -68,7 +68,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.BASE_URL,
   scrollBehavior(to, from, savedPosition) {
     if (to.hash) {
@@ -77,7 +77,6 @@ const router = new VueRouter({
       return { x: 0, y: 0 }
     }
   },
-  mode: 'history',
   routes,
 })
 
